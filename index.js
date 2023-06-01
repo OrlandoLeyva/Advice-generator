ef7089f85e0de17a29184af312dcaf57bbac2ea0
orlando leyva <leyvaorlandomx08@gmail.com>
2023-06-01 13:43:21 -0700

    repeated advice bug fixed

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 
 // HTML ELEMENTS
 
+caches.keys().then(keyList => Promise.all(keyList.map(key => caches.delete(key))));
+
 const adviceNumberEl = document.getElementById('advice-number')
 const adviceEl = document.getElementById('advice')
 const newAdviceButton = document.getElementById('new-advice-button')
@@ -10,15 +12,25 @@ const newAdviceButton = document.getElementById('new-advice-button')
 // API SETTINGS
 const API_URL = 'https://api.adviceslip.com/advice'
 
+let previousAdvice = {}
+
 // FETCH A NEW ADVICE AND RENDER IT
-newAdviceButton.addEventListener('click', async ()=>{
+newAdviceButton.addEventListener('click', fetchAdvice)
+
+async function fetchAdvice(){
     adviceEl.innerText = 'Loading Advice...'
     newAdviceButton.disabled = true
     const res = await fetch(API_URL);
     const data = await res.json()
+    
     const adviceObj = data.slip
-    adviceNumberEl.innerText = `Advice # ${adviceObj.id}`
-    adviceEl.innerText = adviceObj.advice
-    newAdviceButton.disabled = false
-})
+    if (adviceObj.id == previousAdvice.id){
+        await fetchAdvice()
+    } else {
+        adviceNumberEl.innerText = `Advice # ${adviceObj.id}`
+        adviceEl.innerText = adviceObj.advice
+        previousAdvice = adviceObj
+        newAdviceButton.disabled = false
+    }
+}
 
